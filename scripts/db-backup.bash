6e13b0582b9045548f202f7be7bf9b8385fd4f45
Jonah Greenthal <jonah@jonahgreenthal.com>
2023-09-10 17:47:41 -0500

    compress and redirect the backup within the script

--- scripts/db-backup.bash
+++ scripts/db-backup.bash
@@ -2,4 +2,17 @@
 
 DB_URL_WITHOUT_JDBC=$(echo $REINSTEIN_DB_URL | cut -d ":" -f "2-")
 
-PGPASSWORD=$REINSTEIN_DB_PASSWORD pg_dump --dbname=$DB_URL_WITHOUT_JDBC --user=$REINSTEIN_DB_USERNAME
+if [[ $# -eq 0 ]]; then
+	DESTINATION_DIRECTORY=~
+else
+	DESTINATION_DIRECTORY=$1
+fi
+DESTINATION_FILE=$DESTINATION_DIRECTORY/reinstein-order-$(date +%Y-%m-%d).sql.gz
+
+if [[ -e $DESTINATION_FILE ]]; then
+	echo "$DESTINATION_FILE already exists; doing nothing rather than overwriting" 1>&2
+	exit 1
+else
+	PGPASSWORD=$REINSTEIN_DB_PASSWORD pg_dump --dbname=$DB_URL_WITHOUT_JDBC --user=$REINSTEIN_DB_USERNAME | gzip > $DESTINATION_FILE
+	echo "Wrote database backup to $DESTINATION_FILE"
+fi
